6da7b94555e668e82d9e7d333b6de59a3c3e431c
TERMZL0ckd0wn <128064869+TERMZL0ckd0wn@users.noreply.github.com>
2025-07-12 11:40:05 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# ZeroTier Binary Builder
-Automatically build static ZeroTier binaries from source.
+# ZeroTier-One Void Linux
+This project is for installing ZeroTierOne on Void Linux or simillar Linux distributions that uses "runit" as its init system.
 
-#### Supported Architectures
-- x86_64
-- arm64
+### Installing
+Quick Install :
+```terminal
+curl https://raw.githubusercontent.com/TERMZL0ckd0wn/ZeroTierOne-Void-Linux/refs/heads/install.sh | sudo sh
+```
+
+### Special Thanks
+To the [original repository](https://github.com/rafalb8/ZeroTierOne-Static) of this fork.
+Also, the Void Linux and runit docs for helping me creating the service files for "runit".
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Downloading zerotier one binary..."
+mkdir -p /bin/zerotier-one && cd /bin/zerotier-one
+curl -LJ https://github.com/TERMZL0ckd0wn/ZeroTierOne-Void-Linux/releases/latest/download/zerotier-one-x86_64.tar.gz \
+    | tar --strip-components=1 -xzf -
+
+echo "Configuring zerotier one..."
+# Binary will be run as a user service, but needs root, so add permission to run sudo without password for zerotier-one
+echo "%wheel ALL=(ALL) NOPASSWD: /bin/zerotier-one" | sudo tee /etc/sudoers.d/zerotier 1> /dev/null
+
+mkdir -p /etc/sv/zerotier
+
+cat <<EOF > /etc/sv/zerotier/run
+#!/bin/bash
+exec 2>&1
+exec zerotier-one
+EOF
+
+echo "Enabling the zerotier one service..."
+ln -s /etc/sv/zerotier /var/service
+
+echo "Zerotier is installed and running!"
+echo "binary directory is at /bin"
